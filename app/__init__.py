b727a0963a412115f146c90fe4663e972dc8f8e2
khantil-thummar <khantilthummar@gmail.com>
2025-05-28 00:37:19 +0530

    code optimization and training models creation

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+"""
+Binary Signal Generator for Forex Trading
+
+A comprehensive toolkit for generating binary trading signals using
+machine learning and deep learning approaches.
+"""
+
+__version__ = '1.0.0' 
